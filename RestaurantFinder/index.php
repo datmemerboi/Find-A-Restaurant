25222b27a3bb205d8e7d40ccb49b3176fbc2d0b0
datmemerboi <shravanssd@gmail.com>
2019-10-17 20:27:19 +0530

    Fourth Commit

--- RestaurantFinder/index.php
+++ RestaurantFinder/index.php
@@ -5,37 +5,37 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta name="viewport" content="width=device-width, initial-scale=0.7">
-	<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Quicksand" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<title>Find Your Restaurant</title>
 	<style type="text/css">
 		html {
 			font-family: Verdana, Geneva, sans-serif;
-			font-family: Quicksand;
 			font-size: 19px;
 		}
 		body {
 			background-color: #000;
 			color:white;
+			max-width: 100vw;
 		}
 		#user-half {
 			width: 48%;
-			height: 96%;
+			height: 100vh;
 			padding: 1%;
 			position: absolute;
-			background-color: #101010;
 			top: 0;
 			left: 0;
+			background-color: #101010;
 		}
 		.btn{
 			background-color: #E8E8E8;
-			height: 44px;
-			width: 12%;
+			height: 7vmin;
+			width: 12vmin;
 			position: relative;
 			border-width: 0;
 			outline: none;
 			color: #000;
-			font-family: Quicksand;
+			font-family: Verdana, Geneva, sans-serif;
 			font-size: 14px;
 			font-weight: bold;
 		}
@@ -43,18 +43,62 @@
 			background-color: #C0C0C0;
 			cursor: pointer;
 		}
-		.logout {
-			left: 40em;
-			top: -4em;
+		.logoutBtn {
+			position: relative;
+			left: 40vw;
+			top: -7vh;
 		}
 		#bookings {
-			/*background-color: rgba(73, 240, 5, 0.5);*/
 			background-color: #2C9B00;
-			width: 80%;
+			width: 36vw;
 			position: relative;
-			left: 8%;
+			left: 8vmin;
 			height: auto;
 			padding: 1%;
+			padding-left: 4%;
+		}
+		#search-half{
+			width: 48%;
+			height: 100vh;
+			padding: 1%;
+			position: absolute;
+			top: 0;
+			left: 50%;
+		}
+		#search-bar {
+			background-color: #F0F0F0;
+			width: 38vw;
+			position: relative;
+			left: 2vw;
+			top: 17vh;
+			height: auto;
+			padding: 2%;
+			border: none;
+			outline: none;
+			color: black;
+			font-size: 20px;
+		}
+		.searchBtn {
+			width: 10vmin;
+			position: relative;
+			top: 16.7vh;
+			left: -3.5vw;
+		}
+		#search-result {
+			width: 38vw;
+			height: auto;
+			padding: 2%;
+			position: relative;
+			top: 30vmin;
+			left: 2vw;
+			background-color: #101010;
+			border: none;
+			outline: none;
+			align-content: center;
+			text-align: center;
+		}
+		a {
+			color: #005DFF;
 		}
 	</style>
 </head>
@@ -67,27 +111,31 @@
 				}
 			?> </b>!
 		</p>
-		<button class="btn logout" onclick=" window.location.href='logout.php';"><label>Log Out</label></button>
+		<button class="btn logoutBtn" onclick=" window.location.href='logout.php';"><label>Log Out</label></button>
 		<div id="bookings">
 			<p>Your Upcoming Bookings:</p>
 		</div>
 	</div>
-
-		<!-- <script>
-			window.onload = function() {
-			var PlaceholderIntervalVar = setInterval(function() {
-				var placeholderTimeoutVar = setTimeout(function() {
-					document.getElementById("search-bar").placeholder = "Search Restaurant Name";
-				}, 1000)
-				var placeholderTimeoutVar = setTimeout( function(){
-					document.getElementById("search-bar").placeholder = "Search Restaurant Area";
-				}, 3000)
-			}, 6000);
-		}
-		modalCloseBtn.onclick = function() {
-			modal.style.display = "none";
-			// show = false;
-		}
-		</script> -->
+	<div id="search-half">
+		<form action="" method="POST">
+			<input type="text" id="search-bar" name="search-bar" placeholder="Search">
+			<button class="btn searchBtn"><i class="fa fa-search"></i></button>
+		</form>
+		<div id="search-result">
+			<?php searchrest($conn); ?>
+		</div>
+	</div>
 </body>
+<script>
+	window.onload = function() {
+	var PlaceholderIntervalVar = setInterval(function() {
+		var placeholderTimeoutVar = setTimeout(function() {
+			document.getElementById("search-bar").placeholder ="Search Restaurant Name";
+		}, 1000)
+		var placeholderTimeoutVar = setTimeout( function(){
+			document.getElementById("search-bar").placeholder = "Search Restaurant Area";
+		}, 4000)
+	},6000);
+	}
+</script>
 </html>
--- RestaurantFinder/searchrest.inc.php
+++ RestaurantFinder/searchrest.inc.php
@@ -4,11 +4,10 @@ if(!function_exists('searchrest'))
 {
 	function searchrest($conn)
 	{
-		$returnArray = array();
 
-		if( isset($_POST['search']) && !empty($_POST['search']))
+		if( isset($_POST['search-bar']) && !empty($_POST['search-bar']))
 		{
-			$search = $_POST['search'];
+			$search = $_POST['search-bar'];
 			$sql2 = "SELECT * FROM `joints` WHERE name LIKE '%".$search."%'";
 			$run_sql2 = mysqli_query($conn,$sql2);
 			$num_run_sql2 = mysqli_num_rows($run_sql2);
@@ -16,25 +15,21 @@ if(!function_exists('searchrest'))
 			if($num_run_sql2 == 0)
 			{
 				print_r("No such restautrants!");
-				// array_push($returnArray, "No such restautrants!");
 			}
 			else while($row = mysqli_fetch_assoc($run_sql2))
 			{
-				// array_push($returnArray,$row['name'],$row['address'],$row['contact']);
+				echo "<a href='".$row['name'].".php' target='_blank'>"; print_r($row['name']); echo "</a>"; 
 				print_r("<br>");
-				echo "<a href='../reslist/".$row['name'].".php'>"; print_r($row['name']); echo "</a>"; print_r("<br>");
-				print_r($row['address']);print_r("<br>");
-
+				print_r($row['address']);
+				print_r("<br>");
 				if($row['contact']==NULL){	print_r("Contact not updated yet.");print_r("<br>");	}
 				else{	print_r($row['contact']);print_r("<br>");	}
 			}
 		}
-		if(empty($_POST['search']))
+		if(empty($_POST['search-bar']))
 		{
 			print_r("Enter name to search..");
-			// array_push( $returnArray, "Enter search name..");
 		}
-		return($returnArray);
 	}
 }
 ?>
